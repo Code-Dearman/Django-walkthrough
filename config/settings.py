bd887704cbd360f9a369578edb77521e2ff33a7a
Isaac Dearman <isaac.dearman@gmail.com>
2024-11-12 11:02:57 +0000

    add heroku dependancies and files

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-21=9g%q#m+&+&jgl*y0s)so0kta59putd$+be5rpxxk_i#$ptw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["8000-codedearman-djangowalkt-q697sgwfjzi.ws.codeinstitute-ide.net"]
+ALLOWED_HOSTS = ["8000-codedearman-djangowalkt-q697sgwfjzi.ws.codeinstitute-ide.net",
+'.herokuapp.com']
 
 
 # Application definition
